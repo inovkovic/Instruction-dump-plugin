ba82d6237cd25a3817d5c762c700cc70f9839d11
inovkovic <igornov55@gmail.com>
2015-12-14 14:49:44 +0100

    Added support for recording register changes

--- InstructionDump.c
+++ InstructionDump.c
@@ -1,28 +1,38 @@
 #include <windows.h>
 #include <stdio.h>
-#include <string.h>
+#include <string>
+#include <vector>
+#include <algorithm>
 
 #include "Plugin.h"
+
+struct Configuration {
+	char logName[10];
+	uint numberOfInstructions;	// how many instructions to read
+	uint numberOfJumps;			// how deep is recursion going to be
+	bool recordEntrance;		// whether to record steping in CALL instruction
+	bool recordRegMod;			// whether to record if registers were modified by user
+	bool recordReg;				// whether to record register values
+};
+
+struct State {
+	std::vector<uint> retAddr;				//	return addresses when CALL is encountered
+	bool stepIn;							//	1 - command was STEP_IN 0 - command was STEP_OVER
+	std::vector<bool> regMod;				//  any register modified by user
+	bool wasCall;							//	last instruction was CALL
+};
 
 HINSTANCE        hinst;                // DLL instance
 HWND             hwmain;               // Handle of main OllyDbg window
 
+Configuration config;
+State		  state;
+
 FILE *file;
 
-uchar *output;		// string of instructions which are going to be written in a file
-uint outputPos;		// last position in output string
-uint outputSize;
-
-//	configuration variables, read at the startup
-uint numberOfInstructions;	// how many instructions to read
-uint numberOfJumps;			// how deep is recursion going to be
-
-uint nextInstructionAddress; // address of instruction after call/jxx instruction
 
 //	function for dumping instructions
 void cdecl recordInstructions(t_disasm *disasm, uint depth);
-
-void writeOutput();
 
 
 BOOL WINAPI DllEntryPoint(HINSTANCE hi,DWORD reason,LPVOID reserved) {
@@ -44,23 +54,19 @@ extc int _export cdecl ODBG_Plugininit(int ollydbgversion,HWND hw,ulong *feature
 
    hwmain=hw;
 
-
-   nextInstructionAddress = 0;  //	setting flag down
-
-
    //	reading configuration file
    file = fopen("instDump.conf", "r");
-   fscanf(file, "%lu %lu", &numberOfInstructions, &numberOfJumps);
+   fscanf(file, "%s %lu %lu %d %d %d", config.logName, &config.numberOfInstructions, &config.numberOfJumps, &config.recordEntrance, &config.recordRegMod, &config.recordReg);
    fclose(file);
 
-   outputSize = numberOfInstructions * numberOfJumps * 2;
-   
-   output = (uchar*) malloc(outputSize);
+   state.stepIn = false;
+   state.wasCall = false;
 
-   outputPos = 0;
 
-  //	dump file
-   file = fopen("instructionDump.txt", "w");
+   // log file
+   file = fopen(config.logName, "w");
+
+   fprintf(file, "%lu %lu %d %d %d\n", config.numberOfInstructions, config.numberOfJumps, config.recordEntrance, config.recordRegMod, config.recordReg);
 
    Addtolist(0,0,"Instruction dump plugin v 0.1");
    Addtolist(0,-1,"  Copyright (C) 2015 Igor Novkovic");
@@ -70,7 +76,6 @@ extc int _export cdecl ODBG_Plugininit(int ollydbgversion,HWND hw,ulong *feature
 
 extc void _export cdecl ODBG_Plugindestroy(void) {
 	fclose(file);
-	free(output);
 };
 
 extc int _export cdecl ODBG_Pluginmenu(int origin,char data[4096],void *item) {
@@ -78,15 +83,13 @@ extc int _export cdecl ODBG_Pluginmenu(int origin,char data[4096],void *item) {
     case PM_MAIN:                      
       strcpy(data,"0 &About");
       return 1;
-
   };
   return 0;                           
 };
 
 extc void cdecl ODBG_Pluginreset(void) {
 	fclose(file);
-	file = fopen("instructionDump.txt", "w");
-	outputPos = 0;
+	file = fopen(config.logName, "w");
 }
 
 
@@ -108,15 +111,42 @@ extc int _export cdecl ODBG_Paused(int reason, t_reg *reg) {
 
 	switch(reason) {
 	case PP_EVENT:
-		if(nextInstructionAddress != 0) {		//	if previous instruction was CALL/JXX
-			if(reg->ip == nextInstructionAddress) // if current instruction is instruction after CALL/JXX instruction
-				fprintf(file, "\n0\n");			// command was step over
-			else								// if current instruction is one pointed to by CALL/JXX
-				fprintf(file, "\n1\n");			// command was step into
 
-			nextInstructionAddress = 0;			// reset flag
+		if(state.wasCall == true) {
+			if(reg->ip == state.retAddr.back())
+				state.stepIn = 0;
+			else {
+				state.stepIn = 1;
+				state.regMod.push_back(false);
+			}
+			state.wasCall = false;
 		}
 
+		if(state.retAddr.size() != 0) {
+			bool modified = state.regMod.back();
+			state.regMod.pop_back();
+
+			if(modified == false && reg->modified == 1) {
+				modified = true;
+				fprintf(file, "1\n");
+			}
+
+			state.regMod.push_back(modified);
+
+			if(reg->ip == state.retAddr.back()) {
+				state.retAddr.pop_back();
+
+				/*if(config.recordRegMod == true)
+					fprintf(file, "%d\n", state.regMod.back());*/
+
+				if(config.recordRegMod == false)
+					fprintf(file, "0\n");
+
+				state.regMod.pop_back();
+			}
+		}
+		
+		
 		srcsize = Readcommand(reg->ip, (char*)cmd); 
 
 		if(srcsize != 0) {
@@ -126,13 +156,12 @@ extc int _export cdecl ODBG_Paused(int reason, t_reg *reg) {
 			if(disasm.error != 0)
 				break;
 
-			if(/*disasm.cmdtype == C_JMC || */disasm.cmdtype == C_CAL) {  // if instruction is CALL/JXX
-				nextInstructionAddress = reg->ip + srcsize;			// set flag
+			if(disasm.cmdtype == C_CAL) {							// if instruction is CALL
+				state.wasCall = true;
+				state.retAddr.push_back(reg->ip + srcsize);			// add new ret address
 				recordInstructions(&disasm, 0);						// dump instructions
+				fprintf(file, "\n");
 			}
-
-			if(outputPos != 0)
-				writeOutput();
 
 			return 1;
 		}
@@ -140,20 +169,16 @@ extc int _export cdecl ODBG_Paused(int reason, t_reg *reg) {
 	return 0;
 };
 
-void writeOutput() {
-	output[outputPos] = '\0';
-	fprintf(file, "%s", output);
-	outputPos = 0;
-}
 
-void cdecl recordInstructions(t_disasm *disasm, uint depth) {	//  Readcommand -> readmemory ?
+void cdecl recordInstructions(t_disasm *disasm, uint depth) {
 	ulong ip = disasm->jmpaddr;						
 	ulong srcsize;
 	uchar cmd[MAXCMDSIZE]; 
+	std::string dump;
 	t_disasm dis;
 	int i, j;
 
-	for(i = 0; i < numberOfInstructions; i++) {
+	for(i = 0; i < config.numberOfInstructions; i++) {
 		srcsize = Readcommand(ip,(char*)cmd);
 
 		if(srcsize != 0) {
@@ -162,19 +187,12 @@ void cdecl recordInstructions(t_disasm *disasm, uint depth) {	//  Readcommand ->
 			if(dis.error != 0)
 				break;
 
-			//fprintf(file, "%s", dis.dump);
+			dump += dis.dump;
 
-			for(j = 0; dis.dump[j] != '\0'; j++) {
-				if(dis.dump[j] == ' ')
-					continue;
-				output[outputPos] = dis.dump[j];
-				outputPos++;
-				if(outputPos == (outputSize -1))
-					writeOutput();
-			}
+			dump.erase(std::remove(dump.begin(), dump.end(), ' '), dump.end());
 
-			if(depth < numberOfJumps) {
-				if(/*dis.cmdtype == C_JMC || */dis.cmdtype == C_CAL)	// if instruction is CALL/JXX
+			if(depth < config.numberOfJumps) {
+				if(dis.cmdtype == C_CAL)							// if instruction is CALL
 					recordInstructions(&dis, depth + 1);			// dump instructions
 			}
 
@@ -183,4 +201,6 @@ void cdecl recordInstructions(t_disasm *disasm, uint depth) {	//  Readcommand ->
 		else
 			break;
 	}
+
+	fprintf(file, "%s", dump.c_str());
 };
